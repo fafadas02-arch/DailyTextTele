697162f17e80ef0d41fbecb62061d674dd9d0b92
fafadas02-arch <fafadas02@gmail.com>
2026-03-03 12:37:27 +0200

    Update send.py

--- send.py
+++ send.py
@@ -3,10 +3,13 @@ import requests
 
 token = os.environ["BOT_TOKEN"]
 chat_id = os.environ["CHAT_ID"]
-
-text = "Привіт! Це щоденне повідомлення 🙂"
+text = "Тестове повідомлення"
 
 url = f"https://api.telegram.org/bot{token}/sendMessage"
 r = requests.post(url, data={"chat_id": chat_id, "text": text})
+
+print("STATUS:", r.status_code)
+print("RESPONSE:", r.text)  # <-- це найважливіше
+
 r.raise_for_status()
 print("Sent OK")
